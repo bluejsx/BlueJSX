4bb55c453b74a4a16a19f522947986d880359979
Momijiichigo <ichigomomiji436@gmail.com>
2021-04-13 18:15:54 +0900

    memo

---
+++ memos/Init_on_browser.js
@@ -0,0 +1,24 @@
+/////
+<elem data-vjsx-init='COMP_ID_1,COMP_ID_2/PARENT_ID' />
+
+/////
+
+const { log, warn, error } = console
+
+const compFuncs = {
+  'COMP_ID_1': ()=>{},
+  'COMP_ID_2': ()=>{},
+  'PARENT_ID': ()=>{},
+}
+
+const initElem = ( elem ) =>{
+  elem.dataset.vjsxInit
+    .split('/')[0].split(',')
+    .forEach(id=>{
+      elem.querySelector(`[data-vjsx-init$='/${id}'`).forEach(initElem)
+      compFuncs[id]?.(elem) ?? warn(`comp function ${id} not found`)
+    })
+  elem.removeAttribute('data-vjsx-init')
+}
+
+document.querySelectorAll('[data-vjsx-init]').forEach(elem=>initElem(elem))
