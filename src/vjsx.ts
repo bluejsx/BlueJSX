60a86f51a45a980216e84d9c37a5acc331267c2a
Momijiichigo <ichigomomiji436@gmail.com>
2021-04-25 18:25:44 +0900

    indentation fix

--- src/vjsx.ts
+++ src/vjsx.ts
@@ -4,46 +4,46 @@ import { SVG_TAG_NAMES } from './data'
 import { useAttr } from './vjsxlib'
 
 Object.defineProperty(EventTarget.prototype, 'on', {
-	value: EventTarget.prototype.addEventListener
+  value: EventTarget.prototype.addEventListener
 })
 /*
 EventTarget.prototype.on = EventTarget.prototype.addEventListener;
 
 Element.prototype.ready = function(func: Function){
-	func(this)
+  func(this)
 }
 */
 
 const disableInnerSetter = (elem: ElementWithCustomProps) =>{
-	
-	Object.defineProperties(elem,{
-		innerHTML:{
-			set(){}
-		},
-		innerText:{
-			set(){}
-		}
-	})
+  
+  Object.defineProperties(elem,{
+    innerHTML:{
+      set(){}
+    },
+    innerText:{
+      set(){}
+    }
+  })
 }
 
 const generateTagName = (name: string): string =>
 name.replace(/[A-Z]/g,'-$&').substring(1).toLowerCase()
 
 const processChild = (element: Element, child: any) =>{
-	if(child instanceof Function) {
-		element.append('')
-		
-		const self = element.childNodes[element.childNodes.length-1] as Text
-		disableInnerSetter(element)
-		const setter = (v: any) =>{
-			self.data=v.toString()
-		}
-		child(setter, element)
-	}else if(child instanceof Array){
-		for(const v of child){
-			processChild(element, v)
-		}
-	}else element.append(child)
+  if(child instanceof Function) {
+    element.append('')
+    
+    const self = element.childNodes[element.childNodes.length-1] as Text
+    disableInnerSetter(element)
+    const setter = (v: any) =>{
+      self.data=v.toString()
+    }
+    child(setter, element)
+  }else if(child instanceof Array){
+    for(const v of child){
+      processChild(element, v)
+    }
+  }else element.append(child)
 }
 
 
@@ -52,61 +52,61 @@ function render<T extends HTMLTagName>(component: T, props: jsxProps, ...childre
 function render<T extends SVGTagName>(component: T, props: jsxProps, ...children: JSXChildren): SVGElementTagNameMap[T] & AdditionalElementProps;
 function render<T extends (...args: any) => any>(component: T, props: jsxProps, ...children: JSXChildren): ReturnType<typeof component>;
 function render (component: HTMLTagName | SVGTagName | Function | any, props: jsxProps, ...children: JSXChildren){
-	props ?? (props = {})
-	let element: Element;
+  props ?? (props = {})
+  let element: Element;
 
-	if(typeof component === 'string'){
-		if(SVG_TAG_NAMES.includes(component)){
-			element = document.createElementNS('http://www.w3.org/2000/svg', component)
-		}else {
-			element = document.createElement(component)
-		}
-	}else if(typeof component === 'function'){
-		//if component is custom element class
-		if(component?.prototype instanceof Element){
-			try{
-				element = new component()
-			}catch(e){
-				customElements.define(
-					generateTagName(component.name),
-					component
-				)
-				element = new component()
-			}
-		}else{
-			//if component is a function which returns Element:
-			element = component({...props, children: [...children]})
-			children.length = 0
-		}
-	}else{
-		throw new Error('using invalid thing used as element tag.')
-	}
-	if(props){
-		for(const key in props){
-			const prop = props[key]
-			if(key==='class') element.classList.value = prop
-			else{
-				if(element instanceof SVGElement){
-					element.setAttribute(key, prop)
-				} else {
-					try{
-						element[key] = prop
-					}catch(e){
-						element.setAttribute(key, prop)
-					}
-				}
-			}
-			
-		}
-	}
-	processChild(element, children)
-	
-	return element;
+  if(typeof component === 'string'){
+    if(SVG_TAG_NAMES.includes(component)){
+      element = document.createElementNS('http://www.w3.org/2000/svg', component)
+    }else {
+      element = document.createElement(component)
+    }
+  }else if(typeof component === 'function'){
+    //if component is custom element class
+    if(component?.prototype instanceof Element){
+      try{
+        element = new component()
+      }catch(e){
+        customElements.define(
+          generateTagName(component.name),
+          component
+        )
+        element = new component()
+      }
+    }else{
+      //if component is a function which returns Element:
+      element = component({...props, children: [...children]})
+      children.length = 0
+    }
+  }else{
+    throw new Error('using invalid thing used as element tag.')
+  }
+  if(props){
+    for(const key in props){
+      const prop = props[key]
+      if(key==='class') element.classList.value = prop
+      else{
+        if(element instanceof SVGElement){
+          element.setAttribute(key, prop)
+        } else {
+          try{
+            element[key] = prop
+          }catch(e){
+            element.setAttribute(key, prop)
+          }
+        }
+      }
+      
+    }
+  }
+  processChild(element, children)
+  
+  return element;
 }
 
 const VJSX = {
-	r: render,
-	Fragment: ({ children }: {children: [Element]}) =>children,
+  r: render,
+  Fragment: ({ children }: {children: [Element]}) =>children,
 }
 
 export default VJSX
@@ -115,37 +115,37 @@ export { useAttr }
 /*
 merge string items if there are continuous string items
 
-	const childList = []
-	let strComp = []
-	let previousChildWasString = false;
-	for(let i=0;i<children.length;i++){
-		const v=children[i]
-		if(typeof v === 'string' || typeof v === 'function'){
-			//when the child was string or function
-			strComp.push(v)
-			if(previousChildWasString){
-				//if previous child & current child are str/func  
-				
-				/*
-				children[i-1]+=v
-				children.splice(i, 1)
-				i--
-				
-			}else{
-				//if previous child was NOT str/func & current child are str/func
-				
-				previousChildWasString=true
-			}
-		}else{
-			//when the child was NOT str/func
+  const childList = []
+  let strComp = []
+  let previousChildWasString = false;
+  for(let i=0;i<children.length;i++){
+    const v=children[i]
+    if(typeof v === 'string' || typeof v === 'function'){
+      //when the child was string or function
+      strComp.push(v)
+      if(previousChildWasString){
+        //if previous child & current child are str/func  
+        
+        /*
+        children[i-1]+=v
+        children.splice(i, 1)
+        i--
+        
+      }else{
+        //if previous child was NOT str/func & current child are str/func
+        
+        previousChildWasString=true
+      }
+    }else{
+      //when the child was NOT str/func
 
-			if(strComp.length!==0) childList.push(strComp) 
-			strComp=[]
-			childList.push(v)
-			
-			previousChildWasString=false
-		} 
-	}
-	let childIndex=0
+      if(strComp.length!==0) childList.push(strComp) 
+      strComp=[]
+      childList.push(v)
+      
+      previousChildWasString=false
+    } 
+  }
+  let childIndex=0
 
-	*/
+  */
--- src/vjsxlib.ts
+++ src/vjsxlib.ts
@@ -1,41 +1,41 @@
 import './@types/vjsx.d'
 
 const valueExists = (value: any): boolean =>{
-	if(value===0) return true
-	return !!value
+  if(value===0) return true
+  return !!value
 }
 
 Object.defineProperties(Element.prototype, {
-	_vf: {
-		value: {} as { [key: string]: Function[] & {value?: any} }
-	},
-	watch: {
-		value: function(name: string, listener: (value: any)=>void){
-			const valueField = this._vf[name]
-			if(!valueField){
-				this._vf[name] = [listener]
-			}else{
-				valueField.push(listener)
-				if(valueExists(valueField.value)) listener(valueField.value)
-			}
-		}
-	}
+  _vf: {
+    value: {} as { [key: string]: Function[] & {value?: any} }
+  },
+  watch: {
+    value: function(name: string, listener: (value: any)=>void){
+      const valueField = this._vf[name]
+      if(!valueField){
+        this._vf[name] = [listener]
+      }else{
+        valueField.push(listener)
+        if(valueExists(valueField.value)) listener(valueField.value)
+      }
+    }
+  }
 })
 
 
 function useAttr<Obj extends Element & AdditionalElementProps, PropName extends string, AttrType>(target: Obj, propName: PropName, defaultValue: AttrType): asserts target is Obj & Record<PropName, AttrType>
 {
-	target._vf[propName] ??= []
-	target._vf[propName].value = defaultValue
-	Object.defineProperty(target, propName, {
-		get(): AttrType{
-			return target._vf[propName].value
-		},
-		set(value: AttrType){
-			target._vf[propName].value = value
-			target._vf[propName].forEach(func=>func(value))
-		}
-	})
-	target[propName] = defaultValue
+  target._vf[propName] ??= []
+  target._vf[propName].value = defaultValue
+  Object.defineProperty(target, propName, {
+    get(): AttrType{
+      return target._vf[propName].value
+    },
+    set(value: AttrType){
+      target._vf[propName].value = value
+      target._vf[propName].forEach(func=>func(value))
+    }
+  })
+  target[propName] = defaultValue
 }
 export { useAttr }
