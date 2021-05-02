d0237f54f96900cb828b2f0f4dde8c8c124de281
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-02 22:32:46 +0900

    trandform example codes into testing form

--- examples/example1/src/components/Main.tsx
+++ examples/example1/src/components/Main.tsx
@@ -1,36 +1,20 @@
 import '@vanillajsx/vjsx'
-import marked from 'marked'
-import hljs from 'highlight.js'
+
 import '../declaration.d'
-import 'github-markdown-css'
-import 'highlight.js/styles/vs2015.css'
+
 import { main } from './Main.module.scss'
 import './container.scss'
 
 import Example from './Example'
-import article from './article.md?raw'
 
 const {log} = console
 
-marked.setOptions({
-  highlight: function (code: string, lang: string) {
-    return hljs.highlightAuto(code, [lang]).value
-  }
-})
-const Main = () =>{
-  const container = (<div class='container'>
-    {
-      marked(article).split('<hr>').map(htmStr=>(
-        <section innerHTML={htmStr} class='markdown-body' />
-      ))
-    }
-  </div>)
-  container.querySelector('#example-result-space').appendChild(<Example />)
-  const self = <div class={main}>
-    {container}
+
+const Main = () =>
+  <div class={main}>
+    <Example />
   </div>
-  return self
-}
+
 
 
 export default Main
