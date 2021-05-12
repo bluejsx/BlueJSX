3cf8b343487d5ca3fdc49d6838e9c9e0e5d95999
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-12 21:00:40 +0900

    formatt

--- components/Text.ts
+++ components/Text.ts
@@ -1,3 +1,7 @@
-const Txt = ({children}: {children?: [string], ref?: [object, string], data?: string}) => new Text(children[0]) as unknown as VJSX.JSX.Element
+const Txt = ({children}: {
+  children?: [string], 
+  ref?: [object, string], 
+  data?: string
+}) => new Text(children[0]) as unknown as VJSX.JSX.Element
 
 export default Txt
