12c782ff77a9f6dda8441a9d524ee83edb3e5ff5
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-12 14:10:11 +0900

    fix type

--- components/Text.ts
+++ components/Text.ts
@@ -1,3 +1,3 @@
-const Txt = () => new Text()
+const Txt = ({children}: {children?: [string], ref?: [object, string], data?: string}) => new Text(children[0]) as unknown as VJSX.JSX.Element
 
 export default Txt
