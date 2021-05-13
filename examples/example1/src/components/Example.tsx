62f8706da77d1a5de617c226017945196f36b3e0
Momijiichigo <ichigomomiji436@gmail.com>
2021-05-13 21:11:30 +0900

    version up

--- examples/example1/src/components/Example.tsx
+++ examples/example1/src/components/Example.tsx
@@ -18,12 +18,6 @@ const Example = ({progValue=0, children=null})=>{
   const self = (
     <div class='t3'>
       <button ref={[refs, 'btn']}>click</button>
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <g fill="white" stroke="green" stroke-width="5">
-          <circle cx="40" cy="40" r="25" />
-          <circle cx="60" cy="60" r="25" />
-        </g>
-      </svg>
       <CustomProgress ref={[refs, 'progress']} max='100' value={progValue}/>
       <Txt ref={[refs, 'percentageText']}>{progValue}</Txt> %
       {children}
